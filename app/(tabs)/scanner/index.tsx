cde2c647c5138a84448eba5ea3c02159dd48e296
Iago Mendes <ibrazmen@oberlin.edu>
2023-11-19 00:15:33 -0500

    Move camera component into scanner screen.

--- app/(tabs)/scanner/index.tsx
+++ app/(tabs)/scanner/index.tsx
@@ -1,10 +1,78 @@
+import { Camera, CameraType } from 'expo-camera'
+import { Button, StyleSheet, Text, View, Alert } from 'react-native'
+import { useIsFocused } from '@react-navigation/native'
+
+import { usePicture } from '../../../contexts/Picture'
 import { Container } from '../../../components/Themed'
-import CameraPreview from '../../../components/CameraPreview'
 
 export default function ScannerScreen() {
+  let camera: Camera | null
+
+  const [permission, requestPermission] = Camera.useCameraPermissions()
+  const isFocused = useIsFocused()
+  const {scanPicture} = usePicture()
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return (
+      <Container>
+        <Text>Loading...</Text>
+      </Container>
+    )
+  }
+
+  if (!isFocused) {
+    // This reloads the camera every time. It would be better to avoid this, but the camera was not
+    // working properly otherwise. I'm leaving it like this temporarily.
+    return null
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <Container>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </Container>
+    )
+  }
+
+  function handleCameraError() {
+    Alert.alert('A camera-related error occurred.')
+  }
+
+  function takePicture() {
+    if (!camera) {
+      return
+    }
+
+    camera.takePictureAsync() 
+      .then(picture => {
+        scanPicture(picture)
+      })
+      .catch(handleCameraError)
+  }
+
   return (
     <Container>
-      <CameraPreview />
+      <View style={styles.cameraContainer}>
+        <Camera style={styles.camera} type={CameraType.back} onMountError={handleCameraError} ref={(ref) => camera = ref}>
+        </Camera>
+        <Button onPress={takePicture} title="Scan" />
+      </View>
     </Container>
   )
 }
+
+const cameraWidth = 350 // used in a 4:3 ratio
+const cameraRatio = 16/9; // 4:3
+const styles = StyleSheet.create({
+  cameraContainer: {
+    width: cameraWidth,
+    height: cameraWidth*cameraRatio,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+})
