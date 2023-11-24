a42ef17962d5956748d3946304f3117651e06f35
Minh Nguyen <minh160302@gmail.com>
2023-11-24 12:02:56 -0500

    integrate OpenCV Lambda

--- contexts/Picture.tsx
+++ contexts/Picture.tsx
@@ -1,52 +1,73 @@
-import { ReactNode, createContext, useState, useContext } from 'react'
-import { CameraCapturedPicture } from 'expo-camera'
-import { router } from 'expo-router'
-import { Alert } from 'react-native';
+import { ReactNode, createContext, useState, useContext } from "react";
+import { CameraCapturedPicture } from "expo-camera";
+import { router } from "expo-router";
+import { Alert } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { Buffer } from "buffer";
 
 type PictureContextType = {
-	picture: CameraCapturedPicture | null
-	scanPicture: (picture: CameraCapturedPicture) => void
-}
+  picture: CameraCapturedPicture | null;
+  scanPicture: (picture: CameraCapturedPicture) => void;
+};
 
-export const PictureContext = createContext({} as PictureContextType)
+export const PictureContext = createContext({} as PictureContextType);
 
 export function usePicture() {
-	const picture = useContext(PictureContext)
-	return picture	
+  const picture = useContext(PictureContext);
+  return picture;
 }
 
 type PictureProviderProps = {
-	children: ReactNode
-}
+  children: ReactNode;
+};
 
-export function PictureProvider({children}: PictureProviderProps) {
-	const [picture, setPicture] = useState<CameraCapturedPicture | null>(null)
+export function PictureProvider({ children }: PictureProviderProps) {
+  const [picture, setPicture] = useState<CameraCapturedPicture | null>(null);
 
-	function scanPicture(picture: CameraCapturedPicture) {
-		if (matchPicture(picture)) {
-			setPicture(picture)
-			router.push('/(tabs)/scanner/viewer')
-		} else {
-			Alert.alert("Picture didn't match!");
-		}
-	}
+  async function scanPicture(picture: CameraCapturedPicture) {
+    const isMatch = await matchPicture(picture);
+    if (isMatch) {
+      setPicture(picture);
+      router.push("/(tabs)/scanner/viewer");
+    } else {
+      Alert.alert("Picture didn't match!");
+    }
+  }
 
-	// Returns whether the picture matched our database or not.
-	function matchPicture(picture: CameraCapturedPicture): boolean {
-		// OpenCV code should go here.
+  // Returns whether the picture matched our database or not.
+  async function matchPicture(picture: CameraCapturedPicture): Promise<any> {
+    const base64 = await FileSystem.readAsStringAsync(picture.uri, {
+      encoding: "base64",
+    });
+    const buffer = Buffer.from(base64, "base64");
 
-		// Temporary code below.
-		return Math.random() < 0.5 ? true : false
-	}
+    await fetch("https://3oaxvxorz3.execute-api.us-east-1.amazonaws.com/Prod/hello/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/jpeg; charset=utf-8",
+      },
+      body: buffer,
+    })
+      .then((response) => {
+        console.log(response);
+        console.log("image uploaded");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-	return (
-		<PictureContext.Provider
-			value={{
-				picture,
-				scanPicture,
-			}}
-		>
-			{children}
-		</PictureContext.Provider>
-	)
+    // Temporary code below.
+    return Math.random() < 0.5 ? true : false;
+  }
+
+  return (
+    <PictureContext.Provider
+      value={{
+        picture,
+        scanPicture,
+      }}
+    >
+      {children}
+    </PictureContext.Provider>
+  );
 }
