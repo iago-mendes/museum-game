11d465f5097466d47461592c46ed7636a91493b5
Iago Mendes <ibrazmen@oberlin.edu>
2023-11-27 06:55:23 -0500

    Style dialogue screen.

--- src/app/dialogue/[paintingId].tsx
+++ src/app/dialogue/[paintingId].tsx
@@ -1,23 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
-import { Button } from 'react-native'
+import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 
 import { Container, Text, View } from '../../components/Themed'
 import { dialoguesRecord } from '../../db/dialogues'
-import { DialogueNode, DialogueOption, PaintingId, isPaintingId } from '../../db/types'
+import { DialogueNode, DialogueOption, isPaintingId } from '../../db/types'
 import { usePlayer } from '../../contexts/Player'
 
 export default function DialogueScreen() {
   const params = useLocalSearchParams<{paintingId?: string}>()
   const [paintingId] = useState(params.paintingId)
+  const scrollViewRef = useRef<ScrollView>(null)
 
   const {player, unlockedPaintings, addUnlockedPainting} = usePlayer()
 
-  let dialogue: DialogueNode | undefined = undefined
-
   const [shownDialogue, setShownDialogue] = useState<DialogueNode[]>([])
   const [shownOptions, setShownOptions] = useState<DialogueOption[]>([])
-  let intervalId: NodeJS.Timeout | undefined = undefined
+
+  let dialogue: DialogueNode | undefined = undefined
+  let loopId: NodeJS.Timeout | undefined = undefined
 
   useEffect(() => {
     if (!paintingId || !isPaintingId(paintingId) || player == 'none') {
@@ -42,12 +43,12 @@ export default function DialogueScreen() {
   }, [paintingId, player])
 
   function startDialogueLoop() {
-    intervalId = setInterval(addNextDialogue, 2000)
+    loopId = setInterval(addNextDialogue, 2000)
   }
 
   function stopDialogueLoop() {
-    if (intervalId) {
-      clearInterval(intervalId)
+    if (loopId) {
+      clearInterval(loopId)
     }
   }
 
@@ -68,6 +69,8 @@ export default function DialogueScreen() {
       setTimeout(() => dialogue?.options && setShownOptions(dialogue.options), 1000)
       return
     }
+
+    scrollViewRef.current?.scrollToEnd()
 
     dialogue = dialogue.next
   }
@@ -91,18 +94,55 @@ export default function DialogueScreen() {
   )
 
   return (
-    <Container>
-      {shownDialogue.map((item, index) => (
-        <View key={index}>
-          <Text>{item.speaker}:</Text>
-          <Text>{item.text}</Text>
-        </View>
-      ))}
-      <View>
-        {shownOptions.length > 0 && shownOptions.map(option => (
-          <Button key={option.key} title={option.prompt} onPress={() => selectOption(option)} />
+    <Container style={styles.screenContainer}>
+      <ScrollView ref={scrollViewRef} style={styles.dialogueContainer}>
+        {shownDialogue.map((item, index) => (
+          <View key={index} style={styles.nodeContainer}>
+            <Text>{item.speaker}:</Text>
+            <Text>{item.text}</Text>
+          </View>
         ))}
-      </View>
+        <View style={styles.optionsContainer}>
+          {shownOptions.length > 0 && shownOptions.map(option => (
+            <TouchableOpacity key={option.key} onPress={() => selectOption(option)} style={styles.optionButton}>
+              <Text style={styles.optionPrompt}>{option.key}) {option.prompt}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    paddingVertical: 10,
+  },
+  dialogueContainer: {
+    width: '100%',
+    paddingHorizontal: 25,
+  },
+  nodeContainer: {
+    width: '100%',
+    marginBottom: 10,
+    backgroundColor: '#ffffff18',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  optionsContainer: {
+    width: '100%',
+    alignItems: 'center',
+    gap: 5,
+  },
+  optionButton: {
+    width: '75%',
+    backgroundColor: '#0a0a58',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  optionPrompt: {
+    fontSize: 20,
+  }
+})
--- src/db/dialogues/chair.ts
+++ src/db/dialogues/chair.ts
@@ -11,7 +11,23 @@ const sitOptionDialogue: DialogueNode = {
 			text: 'Yo you are so real for this',
 			next: {
 				speaker: 'Player 2',
-				text: '*screams* What the heck? You can talk?'
+				text: '*screams* What the heck? You can talk?',
+				next: {
+					speaker: 'Chair',
+					text: 'Nobody ever talks to me, and no one ever asked…. I can indeed talk.',
+					next: {
+						speaker: 'Player 2',
+						text: 'Well I want to talk to you. Where is the guy that was just here?',
+						next: {
+							speaker: 'Chair',
+							text: `Oh him? He was super stressed out about a “problem set due at 5PM”. Looked real dejected, but quickly declared real loudly that he'll go to VENICE to get a breather.`,
+							next: {
+								speaker: 'Player 2',
+								text: '... Venice it is.'
+							}
+						}
+					}
+				}
 			}
 		}
 	}
@@ -28,7 +44,7 @@ export const chairDialogues: PaintingDialogues = {
 	},
 	player2: {
 		speaker: 'Chair',
-		text: "This exhibition has made stand for too long, sit if you agree.",
+		text: "This exhibition has asked me to stand for too long, sit if you agree",
 		options: [
 			{
 				key: 'A',
