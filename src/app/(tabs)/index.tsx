e871c5ef8f8dc568cd768b47f32623f06fd93496
Iago Mendes <ibrazmen@oberlin.edu>
2023-11-28 23:59:40 -0500

    Show paintings for level 1 in map.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,33 @@
-import { Container, Text } from '../../styles/ThemedComponents'
+import { StyleSheet } from 'react-native'
+import { levels } from '../../db/levels'
+import { paintingsInfo } from '../../db/paintingsInfo'
+import { Container, Text, View } from '../../styles/ThemedComponents'
+import { colors } from '../../styles/theme'
 
 export default function MapScreen() {
+
   return (
     <Container>
-      <Text>Map</Text>
+      <View>
+        <Text>Level 1:</Text>
+        <View style={styles.levelPaintingList}>
+          {levels.level1.map(id => (
+            <View key={id} style={styles.paintingContainer}>
+              <Text>{paintingsInfo[id].title}</Text>
+              <Text>{paintingsInfo[id].date}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  levelPaintingList: {
+    gap: 10,
+  },
+  paintingContainer: {
+    backgroundColor: colors.highlight,
+  }
+})
