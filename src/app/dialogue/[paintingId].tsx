d265a4d722f170f3effee1ff4c01c6f1a31093f0
Iago Mendes <ibrazmen@oberlin.edu>
2023-11-28 23:22:40 -0500

    Use next button to control pace of dialogue.

--- src/app/dialogue/[paintingId].tsx
+++ src/app/dialogue/[paintingId].tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 import { Container, Text, View } from '../../styles/ThemedComponents'
 import { dialoguesRecord } from '../../db/dialogues'
@@ -15,15 +16,15 @@ export default function DialogueScreen() {
 
   const {player, unlockedPaintings, addUnlockedPainting} = usePlayer()
 
+  const [dialogue, setDialogue] = useState<DialogueNode | undefined>(undefined)
   const [shownDialogue, setShownDialogue] = useState<DialogueNode[]>([])
   const [shownOptions, setShownOptions] = useState<DialogueOption[]>([])
-
-  let dialogue: DialogueNode | undefined = undefined
-  let loopId: NodeJS.Timeout | undefined = undefined
+  const [showNextButton, setShowNextButton] = useState(true)
+  const [showNextDialogue, setShowNextDialogue] = useState(true)
 
   useEffect(() => {
     if (!paintingId || !isPaintingId(paintingId) || player == 'none') {
-      dialogue = undefined
+      setDialogue(undefined)
       setShownDialogue([])
       return
     }
@@ -31,31 +32,21 @@ export default function DialogueScreen() {
     const dialogueOptions = dialoguesRecord[paintingId]
 
     if (!dialogueOptions.locked || unlockedPaintings.has(paintingId)) {
-      dialogue = dialogueOptions[player]
+      setDialogue(dialogueOptions[player])
     } else {
-      dialogue = dialogueOptions.locked
-    }
-
-    startDialogueLoop()
-
-    return () => {
-      stopDialogueLoop()
+      setDialogue(dialogueOptions.locked)
     }
   }, [paintingId, player])
 
-  function startDialogueLoop() {
-    loopId = setInterval(addNextDialogue, 2000)
-  }
-
-  function stopDialogueLoop() {
-    if (loopId) {
-      clearInterval(loopId)
+  useEffect(() => {
+    if (showNextDialogue) {
+      addNextDialogue()
     }
-  }
+  }, [dialogue, showNextDialogue])
 
   function addNextDialogue() {
+    console.log(dialogue)
     if (!dialogue) {
-      stopDialogueLoop()
       return
     }
 
@@ -66,20 +57,27 @@ export default function DialogueScreen() {
     setShownDialogue(shownDialogue => !dialogue ? shownDialogue : [...shownDialogue, dialogue])
 
     if (dialogue.options) {
-      stopDialogueLoop()
-      setTimeout(() => dialogue?.options && setShownOptions(dialogue.options), 1000)
-      return
+      const optionsCopy = [...dialogue.options]
+      setTimeout(() => setShownOptions(optionsCopy), 1000)
+      setShowNextButton(false)
     }
 
     scrollViewRef.current?.scrollToEnd()
 
-    dialogue = dialogue.next
+    if (dialogue.next) {
+      setDialogue(dialogue.next)
+      setShowNextButton(true)
+    } else {
+      setShowNextButton(false)
+    }
+
+    setShowNextDialogue(false)
   }
 
   function selectOption(option: DialogueOption) {
-    dialogue = option.dialogue
+    setDialogue(option.dialogue)
+    setShowNextDialogue(true)
     setShownOptions([])
-    startDialogueLoop()
   }
 
   if (!paintingId || !isPaintingId(paintingId)) return (
@@ -96,21 +94,29 @@ export default function DialogueScreen() {
 
   return (
     <Container style={styles.screenContainer}>
-      <ScrollView ref={scrollViewRef} style={styles.dialogueContainer}>
-        {shownDialogue.map((item, index) => (
-          <View key={index} style={styles.nodeContainer}>
-            <Text>{item.speaker}:</Text>
-            <Text>{item.text}</Text>
-          </View>
-        ))}
-        <View style={styles.optionsContainer}>
-          {shownOptions.length > 0 && shownOptions.map(option => (
-            <TouchableOpacity key={option.key} onPress={() => selectOption(option)} style={styles.optionButton}>
-              <Text style={styles.optionPrompt}>{option.key}) {option.prompt}</Text>
-            </TouchableOpacity>
+      <View style={styles.dialogueWrapper}>
+        <ScrollView ref={scrollViewRef} style={styles.dialogueContainer}>
+          {shownDialogue.map((item, index) => (
+            <View key={index} style={styles.nodeContainer}>
+              <Text>{item.speaker}:</Text>
+              <Text>{item.text}</Text>
+            </View>
           ))}
-        </View>
-      </ScrollView>
+          <View style={styles.optionsContainer}>
+            {shownOptions.length > 0 && shownOptions.map(option => (
+              <TouchableOpacity key={option.key} onPress={() => selectOption(option)} style={styles.optionButton}>
+                <Text style={styles.optionPrompt}>{option.key}) {option.prompt}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+      {showNextButton && (
+        <TouchableOpacity onPress={() => setShowNextDialogue(true)} style={styles.nextButton}>
+          <Text style={styles.nextContent}>Next</Text>
+          <FontAwesome name="arrow-right" style={styles.nextContent} />
+        </TouchableOpacity>
+      )}
     </Container>
   )
 }
@@ -118,10 +124,20 @@ export default function DialogueScreen() {
 const styles = StyleSheet.create({
   screenContainer: {
     paddingVertical: 10,
+    paddingHorizontal: 25,
+    justifyContent: 'flex-start'
+  },
+  dialogueWrapper: {
+    width: '100%',
+    height: '90%',
+    borderWidth: 2,
+    borderColor: colors.highlight,
+    borderRadius: 10,
   },
   dialogueContainer: {
     width: '100%',
-    paddingHorizontal: 25,
+    padding: 5,
+    paddingBottom: 20,
   },
   nodeContainer: {
     width: '100%',
@@ -135,6 +151,7 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     gap: 5,
+    marginBottom: 20,
   },
   optionButton: {
     width: '75%',
@@ -147,5 +164,21 @@ const styles = StyleSheet.create({
     fontSize: fontSizes.large,
     color: colors.background,
     fontWeight: 'bold',
-  }
+  },
+  nextButton: {
+    backgroundColor: colors.primary,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    alignSelf: 'flex-end',
+    marginTop: 10,
+  },
+  nextContent: {
+    color: colors.background,
+    fontWeight: 'bold',
+    fontSize: fontSizes.large,
+  },
 })
