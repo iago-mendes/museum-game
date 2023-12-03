f76755dc17b2cbc9c62bb51f46fffe53d995a79a
Minh Nguyen <minh160302@gmail.com>
2023-12-02 22:40:30 -0500

    Scan QR code and routing

--- src/app/(tabs)/scanner/index.tsx
+++ src/app/(tabs)/scanner/index.tsx
@@ -1,16 +1,19 @@
-import { Camera, CameraType } from 'expo-camera'
+import { BarCodeScanningResult, Camera, CameraType } from 'expo-camera'
 import { Button, StyleSheet, Text, View, Alert } from 'react-native'
 import { useIsFocused } from '@react-navigation/native'
+import { BarCodeScanner } from "expo-barcode-scanner"
+import { router } from 'expo-router'
 
-import { usePicture } from '../../../contexts/Picture'
 import { Container } from '../../../styles/ThemedComponents'
+import { useState } from 'react'
+import { isPaintingId } from '../../../db/paintingIds'
 
 export default function ScannerScreen() {
-  let camera: Camera | null
+  let camera: Camera | null;
 
-  const [permission, requestPermission] = Camera.useCameraPermissions()
-  const isFocused = useIsFocused()
-  const {scanPicture} = usePicture()
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+  const isFocused = useIsFocused();
 
   if (!permission) {
     // Camera permissions are still loading
@@ -38,20 +41,21 @@ export default function ScannerScreen() {
   }
 
   function handleCameraError() {
-    Alert.alert('A camera-related error occurred.')
+    Alert.alert("A camera-related error occurred.");
   }
 
-  function takePicture() {
-    if (!camera) {
-      return
+  // Scan code when hover phone in front of the QR code
+  const handleBarCodeScanned = ({ type, data }: BarCodeScanningResult) => {
+    setScanned(true);
+    if (type == BarCodeScanner.Constants.BarCodeType.qr) {
+      if (isPaintingId(data))
+        router.push(`/dialogue/${data}`);
+      else
+        Alert.alert(`Wrong painting scanned (${data})`);
+    } else {
+      Alert.alert(`Wrong type of qr code scanned (${type})`);
     }
-
-    camera.takePictureAsync() 
-      .then(picture => {
-        scanPicture(picture)
-      })
-      .catch(handleCameraError)
-  }
+  };
 
   return (
     <Container>
@@ -60,13 +64,22 @@ export default function ScannerScreen() {
           style={styles.camera}
           type={CameraType.back}
           onMountError={handleCameraError}
-          ref={(ref) => camera = ref}
-          ratio="16:9"
+          ref={(ref) => (camera = ref)}
+          ratio="1:1"
+          barCodeScannerSettings={{
+            barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+          }}
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         />
-        <Button onPress={takePicture} title="Scan" />
+        {scanned && (
+          <Button
+            title={"Tap to Scan Again"}
+            onPress={() => setScanned(false)}
+          />
+        )}
       </View>
     </Container>
-  )
+  );
 }
 
 const cameraWidth = 350 // used in a 4:3 ratio
