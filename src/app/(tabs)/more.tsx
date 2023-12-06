2a5b87954436d7d83e116f94653416dec3b13a01
Iago Mendes <ibrazmen@oberlin.edu>
2023-12-06 08:51:11 -0500

    Style more screen.

--- src/app/(tabs)/more.tsx
+++ src/app/(tabs)/more.tsx
@@ -1,13 +1,14 @@
 import { Container } from '../../styles/ThemedComponents'
 import { usePlayer } from '../../contexts/Player'
 import { Button } from 'react-native'
+import { ButtonWithIcon } from '../../components/ButtonWIthIcon'
 
 export default function MoreScreen() {
   const {reset} = usePlayer()
 
   return (
     <Container>
-      <Button title="Reset game" onPress={() => reset()} />
+      <ButtonWithIcon text="Reset game" icon="refresh" onPress={() => reset()} />
     </Container>
   )
 }
