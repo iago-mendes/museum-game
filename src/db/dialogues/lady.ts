02a5dd69de96fa5a1ae269d252efe5098c218d2c
Ben Yost <byost@oberlin.edu>
2023-12-08 06:59:08 -0500

    lady completed

---
+++ src/db/dialogues/lady.ts
@@ -0,0 +1,10 @@
+import { PaintingDialogues } from '.'
+
+export const lady: PaintingDialogues = {
+    locked: {
+        speaker: "Narrator",
+        text: "The lady sits, refined and dignified, before you. Her eyes razor sharp, as if boring into your soul, yet clearly she is utterly uninterested in anything you might have to say."
+    },
+    player1:{},
+    player2: {}
+}
